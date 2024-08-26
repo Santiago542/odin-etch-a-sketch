7b61268eed1ba5af25954e0f1a9c8749afb2d9f1
Santiago542 <santiagocortes464@gmail.com>
2024-08-26 14:03:14 +0200

    Fix the algorithm that creates the grid display

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 function createGrid(size) {
     const container = document.querySelector(".container");
+    const computedStyle = window.getComputedStyle(container);
+    const squareSize = (parseInt(computedStyle.width) - parseInt(computedStyle.padding) * 2 - parseInt(computedStyle.borderWidth) * 2) / size;
+    
+    container.innerHTML = "";
 
-    for(let i = 0; i < size; i++) {
+    for(let i = 0; i < size * size; i++) {
         const div = document.createElement("div");
-        div.textContent = i;
-        div.style.cssText = "border: 3px solid black; width: 200px; height: 200px";
+        div.style.cssText = `border: 1px solid black; width: ${squareSize}px; height: ${squareSize}px`;
         div.addEventListener("mouseenter", () => {
             div.style.backgroundColor = "black";
-        })
+        });
         container.appendChild(div);
-        }
-}
-
-for(let i = 0; i < 16; i++) {
-    
+    }
 }
 
 const resizeButton = document.querySelector("#resize");
 
 resizeButton.addEventListener("click", () => {
-    let newSize = parseInt(prompt("Enter new size between 16 and 100: "));
-    while(newSize < 16 || newSize > 100) {
+    let newSize = parseInt(prompt("Enter new grid size between 16 and 100: "));
+    while(newSize < 4 || newSize > 100) {
         newSize = parseInt(prompt("The new grid size must be between 16 and 100: "));
     }
 
